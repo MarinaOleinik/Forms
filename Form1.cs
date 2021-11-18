c87dff54a73b4c05d884a228e67f30434c79ce77
marina.oleinik <marina.oleinik@HP-CZC6297BB4>
2021-11-18 10:19:32 +0200

    uuendatud

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,6 +15,13 @@ namespace Forms
     {
         TreeView tree;
         Button btn;
+        Label lbl;
+        CheckBox box_lbl, box_btn;
+        RadioButton r1, r2;
+        TextBox txt_box;
+        PictureBox picture;
+        TabControl tabControl;
+        TabPage page1, page2, page3;
         public Form1()
         {
             this.Height = 500;
@@ -24,6 +32,27 @@ namespace Forms
             tree.AfterSelect += Tree_AfterSelect;
             TreeNode tn = new TreeNode("Elemendid");
             tn.Nodes.Add(new TreeNode("Nupp-Button"));
+            //button
+            btn = new Button();
+            btn.Text = "Vajuta siia";
+            btn.Location = new Point(200, 100);
+            btn.Height = 40;
+            btn.Width = 120;
+            btn.Click += Btn_Click;
+            //
+            tn.Nodes.Add(new TreeNode("Silt-Label"));
+            //label
+            lbl = new Label();
+            lbl.Text = "Tarkvara arendajad";
+            lbl.Size = new Size(150, 30);
+            lbl.Location = new Point(150, 200);
+            //
+            tn.Nodes.Add(new TreeNode("Märkeruut-CheckBox"));
+            tn.Nodes.Add(new TreeNode("Radionupp-Radiobutton"));
+            tn.Nodes.Add(new TreeNode("Tekstkast-TextBox"));
+            tn.Nodes.Add(new TreeNode("PictureBox"));
+            tn.Nodes.Add(new TreeNode("Kaart-TabControl"));
+            tn.Nodes.Add(new TreeNode("MessageBox"));
 
             tree.Nodes.Add(tn);
             this.Controls.Add(tree);
@@ -33,13 +62,114 @@ namespace Forms
         {
             if (e.Node.Text=="Nupp-Button")
             {
-                btn = new Button();
-                btn.Text = "Vajuta siia";
-                btn.Location = new Point(200, 100);
-                btn.Height = 40;
-                btn.Width = 120;
-                btn.Click += Btn_Click;
+                
                 this.Controls.Add(btn);
+            }
+            else if(e.Node.Text== "Silt-Label")
+            {
+                
+                this.Controls.Add(lbl);
+            }
+            else if (e.Node.Text== "Märkeruut-CheckBox")
+            {
+                box_btn = new CheckBox();
+                box_btn.Text = "Näita nupp";
+                box_btn.Location = new Point(200, 30);
+                this.Controls.Add(box_btn);
+                box_lbl = new CheckBox();
+                box_lbl.Text = "Näita silt";
+                box_lbl.Location = new Point(200, 70);
+                this.Controls.Add(box_lbl);
+                box_btn.CheckedChanged += Box_btn_CheckedChanged;
+            }
+            else if (e.Node.Text== "Radionupp-Radiobutton")
+            {
+                r1 = new RadioButton();
+                r1.Text = "nupp vasakule";
+                r1.Location = new Point(300, 30);
+                r1.CheckedChanged += new EventHandler(Radiobuttons_Changed);
+                r2 = new RadioButton();
+                r2.Text = "nupp paremale";
+                r2.Location = new Point(300, 70);
+                r2.CheckedChanged += new EventHandler(Radiobuttons_Changed);
+                this.Controls.Add(r1);
+                this.Controls.Add(r2);
+            }
+            else if (e.Node.Text== "Tekstkast-TextBox")
+            {
+                txt_box = new TextBox();
+                txt_box.Multiline = true;
+                txt_box.Text = "Failist";
+                txt_box.Location = new Point(300,300);
+                txt_box.Width = 100;
+                txt_box.Height = 100;
+            }
+            else if (e.Node.Text== "PictureBox")
+            {
+                picture = new PictureBox();
+                picture.Image = new Bitmap("image.png");
+                picture.Location = new Point(500,10);
+                picture.Size = new Size(90, 90);
+                picture.SizeMode = PictureBoxSizeMode.Zoom;
+                picture.BorderStyle = BorderStyle.Fixed3D;
+                this.Controls.Add(picture);
+            }
+            else if (e.Node.Text== "Kaart-TabControl")
+            {
+                
+                tabControl = new TabControl();
+                tabControl.Location = new Point(300, 300);
+                tabControl.Size = new Size(200, 100);
+                page1 = new TabPage("Esimene");
+                page2 = new TabPage("Teine");
+                page3 = new TabPage("Kolmas");
+                tabControl.Controls.Add(page1);
+                tabControl.Controls.Add(page2);
+                tabControl.Controls.Add(page3);
+                tabControl.SelectedIndex = 2;//0,1,2
+
+                this.Controls.Add(tabControl);
+            }
+            else if (e.Node.Text=="MessageBox")
+            {
+                MessageBox.Show("MessageBox", "Kõige lihtsam aken");
+                var answer=MessageBox.Show("Tahad InputBoxi näha?", "Aken koos nupudega", MessageBoxButtons.YesNo);
+                if (answer==DialogResult.Yes)
+                {
+                    string text=Interaction.InputBox("Sisesta siia mingi tekst", "InputBox", "Mingi tekst");
+                    if (MessageBox.Show("Kas tahad tekst saada Tekskastisse?","Teksti salvestamine",MessageBoxButtons.OKCancel)==DialogResult.OK)
+                    {
+                        lbl.Text = text;
+                        Controls.Add(lbl);
+                    }
+                }
+
+            }
+
+        }
+
+        private void Radiobuttons_Changed(object sender, EventArgs e)
+        {
+            if (r1.Checked)
+            {
+                btn.Location = new Point(150, 100);
+
+            }
+            else if (r2.Checked)
+            {
+                btn.Location = new Point(400, 100);
+            }
+        }
+
+        private void Box_btn_CheckedChanged(object sender, EventArgs e)
+        {
+            if (box_btn.Checked)
+            {
+                Controls.Add(btn);
+            }
+            else
+            {
+                Controls.Remove(btn);
             }
         }
 
@@ -48,10 +178,14 @@ namespace Forms
             if (btn.BackColor == Color.Blue)
             {
                 btn.BackColor = Color.Red;
+                lbl.BackColor = Color.Green;
+                lbl.ForeColor = Color.White;
             }
             else
             {
                 btn.BackColor = Color.Blue;
+                lbl.BackColor = Color.White;
+                lbl.ForeColor = Color.Green;
             }
         }
     }
